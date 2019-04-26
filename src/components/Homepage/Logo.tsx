ca8c3f544538198be7a765b6db9de3b0fca9f327
Kristina <kristina.kolesarova@globallogic.com>
2019-04-26 11:37:11 +0200

    modified

--- src/components/Homepage/Logo.tsx
+++ src/components/Homepage/Logo.tsx
@@ -1,7 +1,7 @@
-import React, { FC } from "react";
+import React from "react";
 import logo from "../../static/svg/gilbert-color-font@3x.png";
 
-const Logo: FC = () => (
+const Logo = () => (
   <div className="hidden lg:flex flex-row justify-center m-auto pt-32 pb-32 md:flex ">
     <img src={logo} className="w-2/5 lg:w-1/5" />
     <h1 className="font-gilbert text-4xl w-2/5 lg:w-1/5">
