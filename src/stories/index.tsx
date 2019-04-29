66778d8c2af228277502326686d42ea30df40589
pavol.kiss@akademiasovy.sk <pavol.kiss@akademiasovy.sk>
2019-04-29 09:12:13 +0200

    added header to storybook

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -3,5 +3,7 @@ import { storiesOf } from "@storybook/react";
 import Example from "../components/Example";
 import "../style/index.css";
 import "../style/fonts.css";
+import Header from "../components/Header/Header";
 
 storiesOf("Example", module).add("example", () => <Example />);
+storiesOf("Header", module).add("responsive header", () => <Header />);
