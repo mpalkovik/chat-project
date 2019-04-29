d74daad0fc53aa97c7acd64d8d09899992809920
Kristina <kristina.kolesarova@globallogic.com>
2019-04-29 12:53:50 +0200

    added oval images

--- src/components/Homepage/HomepageList.tsx
+++ src/components/Homepage/HomepageList.tsx
@@ -5,6 +5,10 @@ import smallChildrenIcon from "../../static/svg/icon-therapy-type-deti-do-3-roko
 import childrenIcon from "../../static/svg/icon-therapy-type-deti-4-15.svg";
 import adultIcon from "../../static/svg/icon-therapy-type-pre-dospelych.svg";
 import individualIcon from "../../static/svg/icon-therapy-type-pre-dospelych-copy.svg";
+import blueOval from "../../static/svg/blueOval.svg";
+import orangeOval from "../../static/svg/orangeOval.svg";
+import pinkOval from "../../static/svg/pinkOval.svg";
+import greenOval from "../../static/svg/greenOval.svg";
 
 const HomepageList = () => (
   <div className="flex flex-col items-center m-0 bg-white relative p-6 lg:m-12 text-center">
@@ -18,16 +22,23 @@ const HomepageList = () => (
     <div className="flex flex-col relative w-full lg:flex-row justify-center">
       <Icon
         src={smallChildrenIcon}
+        url={blueOval}
         title="Pre deti do 3 rokov"
         to="smallChildren"
       />
       <Icon
         src={childrenIcon}
+        url={orangeOval}
         title="Pre deti od 4 do 12 rokov"
         to="children"
       />
-      <Icon src={adultIcon} title="Pre dospelých" to="adults" />
-      <Icon src={individualIcon} title="Individúalne hodiny" to="individials" />
+      <Icon src={adultIcon} title="Pre dospelých" to="adults" url={pinkOval} />
+      <Icon
+        src={individualIcon}
+        title="Individúalne hodiny"
+        to="individials"
+        url={greenOval}
+      />
     </div>
     <div className="w-64 mt-5 border border-solid rounded-lg bg-white text-black font-black">
       <Button text="Všetky naše arteterapie" />
--- src/components/Homepage/Icon.tsx
+++ src/components/Homepage/Icon.tsx
@@ -1,22 +1,22 @@
 import React, { FC } from "react";
 import { Link } from "@reach/router";
 import styled from "@emotion/styled";
-import oval from "../../static/svg/oval.svg";
 
 interface IProps {
   src: string;
+  url: string;
   title: string;
   to: string;
 }
-const Oval = styled.span`
-  background-image: url(${oval});
+const Oval: any = styled.span`
+  background-image: url(${(props: any) => props.url});
   display: inline-block;
   background-position: center;
   background-repeat: no-repeat;
 `;
-const Icon: FC<IProps> = ({ src, title, to }) => (
+const Icon: FC<IProps> = ({ src, url, title, to }) => (
   <Link className="p-6 no-underline m-3" to={to}>
-    <Oval>
+    <Oval url={url}>
       <img src={src} />
     </Oval>
     <h4 className="w-full font-lato text-center text-black pt-4">{title}</h4>
