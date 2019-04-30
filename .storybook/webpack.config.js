548d1db2b154ff4fb376a1becdbe7f81adaecddd
Peter Guzma <peter.guzma@globallogic.com>
2019-04-30 10:30:31 +0200

    add tailwind support for storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,12 +1,28 @@
 module.exports = ({ config }) => {
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve("awesome-typescript-loader")
-      }
-    ]
-  });
+  config.module.rules.push(
+    {
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve("awesome-typescript-loader")
+        }
+      ]
+    },
+    {
+      test: /\.css$/,
+      loaders: [
+        {
+          loader: "postcss-loader",
+          options: {
+            sourceMap: true,
+            config: {
+              path: "./"
+            }
+          }
+        }
+      ]
+    }
+  );
   config.resolve.extensions.push(".ts", ".tsx");
   return config;
 };
