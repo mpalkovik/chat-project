974dd64c02c5ece00c91d81d763d1e0c86080df3
pavol.kiss@akademiasovy.sk <pavol.kiss@akademiasovy.sk>
2019-04-30 15:53:37 +0200

    Added all version to storybook

---
+++ src/components/Screens.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export const XlargeWrapper = styled.div`
+  width: 1200px;
+`;
+
+export const LargeWrapper = styled.div`
+  width: 992px;
+`;
+
+export const MediumWrapper = styled.div`
+  width: 768px;
+`;
+
+export const SmallWrapper = styled.div`
+  width: 576px;
+`;
--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -4,15 +4,34 @@ import Example from "../components/Example";
 import "../style/index.css";
 import "../style/fonts.css";
 import Header from "../components/Header/Header";
+import {
+  XlargeWrapper,
+  MediumWrapper,
+  LargeWrapper,
+  SmallWrapper
+} from "../components/Screens";
 
 storiesOf("Example", module).add("example", () => <Example />);
 storiesOf("Header", module).add("desktop version", () => (
-  <div className="w-full">
+  <XlargeWrapper>
     <Header />
-  </div>
+  </XlargeWrapper>
 ));
-storiesOf("Header", module).add("mobile version", () => (
-  <div className="lg:w-full">
+
+storiesOf("Header", module).add("notebook version", () => (
+  <LargeWrapper>
     <Header />
-  </div>
+  </LargeWrapper>
+));
+
+storiesOf("Header", module).add("tablet version", () => (
+  <MediumWrapper>
+    <Header />
+  </MediumWrapper>
+));
+
+storiesOf("Header", module).add("mobile version", () => (
+  <SmallWrapper>
+    <Header />
+  </SmallWrapper>
 ));
