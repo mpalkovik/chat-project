4a644269bc75f8a98959d74b2728e73534a030b6
Peter Guzma <peter.guzma@globallogic.com>
2019-05-02 17:44:50 +0200

    add viewport support into storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,15 @@
-import { configure } from "@storybook/react";
+import { configure, addParameters } from "@storybook/react";
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { viewports } from "./viewports";
+
+addParameters({
+  viewport: {
+    viewports: {
+      ...INITIAL_VIEWPORTS,
+      ...viewports,
+    },
+  },
+});
 
 function loadStories() {
   require("../src/stories");
