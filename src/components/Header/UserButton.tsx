e84ee5cc3fb350ebae1dbcab77f52168ad1e7511
PavolKiss <pavol.kiss@akademiasovy.sk>
2019-05-02 14:17:03 +0200

    Added dropdown menu

--- src/components/Header/UserButton.tsx
+++ src/components/Header/UserButton.tsx
@@ -3,6 +3,7 @@ import userIcon from "../../static/svg/userIcon.svg";
 import ImageHeader from "./ImageHeader";
 import UserProfile from "./UserProfile";
 import UserMenu from "./UserMenu";
+import { Link } from "@reach/router";
 
 const UserButton = () => (
   <div className="flex flex-row w-auto lg:mr-10 w-48 flex flex-row">
@@ -11,6 +12,20 @@ const UserButton = () => (
       <ImageHeader src={userIcon} className="flex flex-row h-4 w-4 lg:hidden" />
     </div>
     <UserProfile />
+    <div className="flex flex-col overflow-auto z-10 absolute">
+      <Link to="#" className="userProfileLinks">
+        Some
+      </Link>
+      <Link to="#" className="userProfileLinks">
+        Some
+      </Link>
+      <Link to="#" className="userProfileLinks">
+        Some
+      </Link>
+      <Link to="#" className="userProfileLinks">
+        Some
+      </Link>
+    </div>
   </div>
 );
 
--- src/components/Header/userMenu.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const UserMenu = () => (
-  <div className="hidden lg:inline-block">
-    <h3 className="font-gilbert">FS</h3>
-  </div>
-);
-
-export default UserMenu;
