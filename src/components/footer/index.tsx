4e8249ce00a84067ca39647cddc9f0405ec222ae
erik.ruszinka <erik.ruszinka@gmail.com>
2019-05-02 13:55:15 +0200

    Complete footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,12 +1,26 @@
 import React, { FC } from "react";
 import Logo from "./Logo";
 import ICO from "./Info";
+import Icon from "../Icon";
 import Contacts from "./Contacts";
 import Location from "./Location";
+import Insta from "../../static/svg/instagram.svg";
+import Facebook from "../../static/svg/facebook-square.svg";
 
 const Footer: FC = () => (
   <div className=" flex items-end justify-start z-0 bg-cover-image lg:bg-cover-image">
-    <div className="z-0 bg-oval lg:bg-oval" />
+    <div className="flex lg:social-icons social-icons items-center justify-end">
+      <div className="w-12 mr-1">
+        <a href="">
+          <Icon src={Facebook} />
+        </a>
+      </div>
+      <div className="w-12 ml-1">
+        <a href="">
+          <Icon src={Insta} />
+        </a>
+      </div>
+    </div>
     <div className=" lg:pl-16 z-20">
       <Logo />
       <div className="lg:flex">
@@ -18,6 +32,9 @@ const Footer: FC = () => (
           <Contacts />
         </div>
       </div>
+    </div>
+    <div className="overflow-ovals">
+      <div className="z-0 bg-oval lg:bg-oval" />
     </div>
   </div>
 );
