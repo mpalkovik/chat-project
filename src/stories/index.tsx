1576b4734cea1616d8a6ac0fd77c64606a907248
erik.ruszinka <erik.ruszinka@gmail.com>
2019-05-03 12:50:17 +0200

    Added storybook

---
+++ src/stories/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import Example from "../components/Example";
+import "../style/index.css";
+import "../style/fonts.css";
+import Footer from "../components/footer";
+
+storiesOf("Example", module).add("example", () => <Example />);
+storiesOf("Footer", module).add("responsive footer", () => <Footer />);
