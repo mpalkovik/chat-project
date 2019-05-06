253a25e83d1c26281b99c53e342568aa48481dc9
slavomircesla <slavomir.cesla@akademiasovy.sk>
2019-05-06 10:40:29 +0200

    review 1.5

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,7 +4,6 @@ import Home from "./components/Home";
 import Example from "./components/Example";
 import Counter from "./components/Counter";
 import Testing from "./components/Testing";
-import Therapy from "./components/thumbnailTherapy/Therapy";
 
 export default () => {
   return (
@@ -12,7 +11,6 @@ export default () => {
       <Home path="/" />
       <Example path="foo" />
       <Counter path="counter" />
-      <Therapy path="therapy" />
       <Testing path="testing" />
     </Router>
   );
--- src/components/thumbnailTherapy/Therapy.tsx
+++ src/components/thumbnailTherapy/Therapy.tsx
@@ -1,12 +1,11 @@
-import React, { FC } from "react";
-import { RouteComponentProps } from "@reach/router";
+import React from "react";
 import TherapyButton from "./TherapyButton/TherapyButton";
 import TherapyDescription from "./TherapyDescription/TherapyDescription";
 import TherapyImage from "./TherapyImage/TherapyImage";
 import TherapyName from "./TherapyName/TherapyName";
 import icon from "./TherapyImage/homer.svg";
 
-const Therapy: FC<RouteComponentProps> = () => {
+const Therapy = () => {
   return (
     <div className="container ml-6 ">
       <div className="p-2 w-1/5 h-1/3 m-2 shadow mr-2 mb-2 block ">
