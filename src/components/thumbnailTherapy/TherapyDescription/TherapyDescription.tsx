ae8e45598a9d80c1615967c050f13d93f36cc17c
slavomircesla <slavomir.cesla@akademiasovy.sk>
2019-05-06 09:30:56 +0200

    Review 1.3

---
+++ src/components/thumbnailTherapy/TherapyDescription/TherapyDescription.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+
+export interface IDescribe {
+  text: string;
+}
+
+const TherapyDescription: FC<IDescribe> = ({ text }) => {
+  return (
+    <p className=" text-sm leading-normal font-lato font-normal mb-4">{text}</p>
+  );
+};
+
+export default TherapyDescription;
