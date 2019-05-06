13ef1fada090986d41cacf8dca61e70728f210ca
slavomircesla <slavomir.cesla@akademiasovy.sk>
2019-05-06 09:35:43 +0200

    Revert "Review 1.3"
    This reverts commit ae8e45598a9d80c1615967c050f13d93f36cc17c.

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -3,20 +3,5 @@ import { storiesOf } from "@storybook/react";
 import Example from "../components/Example";
 import "../style/index.css";
 import "../style/fonts.css";
-import Therapy from "../components/thumbnailTherapy/Therapy";
-import TherapyName from "../components/thumbnailTherapy/TherapyName/TherapyName";
-import TherapyDescription from "../components/thumbnailTherapy/TherapyDescription/TherapyDescription";
-import TherapyButton from "../components/thumbnailTherapy/TherapyButton/TherapyButton";
-import TherapyImage from "../components/thumbnailTherapy/TherapyImage/TherapyImage";
-import icon from "../components/thumbnailTherapy/TherapyImage/homer.svg";
 
 storiesOf("Example", module).add("example", () => <Example />);
-
-storiesOf("ThumbnailTherapy", module)
-  .add("Therapy", () => <Therapy />)
-  .add("Image", () => <TherapyImage icon={icon} />)
-  .add("Name", () => <TherapyName text="MAMA CHAT" />)
-  .add("Description", () => (
-    <TherapyDescription text="Kreatívne a uvoľnujúce aktivity, aby ste si oddýchli, spracovali emócie […]" />
-  ))
-  .add("Button", () => <TherapyButton text="Zistiť viac" />);
