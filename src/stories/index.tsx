ac134f4b205f442ff6cb8ee7e43e548a2991367d
PavolKiss <pavol.kiss@akademiasovy.sk>
2019-05-09 10:07:46 +0200

    modified imports

--- src/stories/index.tsx
+++ src/stories/index.tsx
@@ -4,14 +4,13 @@ import { CreditPopup } from "../components/Popup/Storybook/CreditPopup";
 import { ErrorPopup } from "../components/Popup/Storybook/ErrorPopup";
 import { RegistrationPopup } from "../components/Popup/Storybook/RegistrationPopup";
 import { CancelReservationPopup } from "../components/Popup/Storybook/CancelReservationPopup";
+import { Header } from "../components/Header/Header";
 import "../style/index.css";
 import "../style/fonts.css";
-import { Header } from "../components/Header/Header";
 
 storiesOf("Popups", module)
   .add("CreditPopup", () => <CreditPopup />)
   .add("RegistrationModal", () => <RegistrationPopup />)
   .add("CancelReservation", () => <CancelReservationPopup />)
   .add("ErrorPopup", () => <ErrorPopup />);
-storiesOf("Header", module).add("header", () => <Header />);
-
+storiesOf("Header", module).add("Header", () => <Header />);
