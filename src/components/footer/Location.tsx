4c349498bbabdc6523c33e06881e30a818b8a65d
erik.ruszinka <erik.ruszinka@gmail.com>
2019-05-15 14:12:06 +0200

    Fixed responsivity

--- src/components/footer/Location.tsx
+++ src/components/footer/Location.tsx
@@ -3,7 +3,7 @@ import { Icon } from "../Icon";
 import Place from "../../static/svg/icon-place.svg";
 
 export const Location = () => (
-  <div className="ml-6 mr-4 lg:pr-12 lg:pl-8 w-1/2 ">
+  <div className="ml-6 mr-4 lg:pr-12 lg:pl-8">
     <div className="flex w-48 justify-start items-center">
       <div className="mr-3 w-6 flex items-start h-10 ">
         <Icon src={Place} />
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -12,13 +12,13 @@ const OverflowOvals = styled.div`
   position: absolute;
   overflow: hidden;
   width: 293px;
-  height: 409px;
+  height: 489px;
   right: 0px;
 `;
 
 export const Footer: FC = () => (
   <div className=" flex items-end justify-start z-0 bg-cover-image lg:bg-cover-image">
-    <div className="flex lg:social-icons social-icons items-center justify-end">
+    <div className="flex lg:social-icons sm:social-icons social-icons">
       <div className="w-12 mr-1">
         <a href="">
           <Icon src={Facebook} />
@@ -36,14 +36,14 @@ export const Footer: FC = () => (
         <div className="pb-6 pr-8">
           <Info />
         </div>
-        <div className="flex pt-6 pb-12 ">
+        <div className="sm:flex pt-6 pb-12 ">
           <Location />
           <Contacts />
         </div>
       </div>
     </div>
     <OverflowOvals>
-      <div className="z-0 bg-oval lg:bg-oval" />
+      <div className="z-0 bg-oval sm:bg-oval lg:bg-oval" />
     </OverflowOvals>
   </div>
 );
