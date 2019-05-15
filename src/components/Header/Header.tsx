8d04b1a29750c0713cb7447da8e5d5a3fe0cc065
PavolKiss <pavol.kiss@akademiasovy.sk>
2019-05-15 14:03:29 +0200

    added hamburger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import chatImg from "../../static/svg/chatLogo.png";
 import { Menu } from "./Menu";
 import { ImageHeader } from "./ImageHeader";
 import { Navigation } from "./Navigation";
 import { UserProfile } from "./UserProfile";
+import { HamburgerMenu } from "./HamburgerMenu";
+import times from "../../static/svg/times.svg";
 
-export const Header = () => (
-  <div>
-    <div className="flex justify-between items-center content-center h-16 lg:h-12 flex justify-between items-center content-center w-full">
-      <Menu />
-      <ImageHeader src={chatImg} width={73} className="h-6 lg:ml-12" />
-      <Navigation />
-      <UserProfile />
+export const Header = () => {
+  const [openMenu, setOpenMenu] = useState("closed");
+  return (
+    <div>
+      {openMenu === "open" && (
+        <div>
+          <div className="flex justify-end mr-6 mt-4">
+            <ImageHeader src={times} onClick={() => setOpenMenu("closed")} />
+          </div>
+          <HamburgerMenu />
+          <div
+            className="opacity-75 bg-darkblue h-full w-full lg:hidden"
+            onClick={() => setOpenMenu("closed")}
+          />
+        </div>
+      )}
+      <div className="flex justify-between items-center content-center h-16 lg:h-12 w-full">
+        <Menu onClick={() => setOpenMenu("open")} />
+        <ImageHeader src={chatImg} width={73} className="h-6 lg:ml-12" />
+        <Navigation />
+        <UserProfile />
+      </div>
+      <div className="bg-lilac h-px" />
     </div>
-    <div className="bg-lilac h-px" />
-  </div>
-);
+  );
+};
--- src/components/Header/ImageHeader.tsx
+++ src/components/Header/ImageHeader.tsx
@@ -3,9 +3,13 @@ import React, { FC } from "react";
 interface IProps {
   src: string;
   width?: number;
-  className: string;
+  className?: string;
+  onClick?: () => void;
 }
 
-export const ImageHeader: FC<IProps> = ({ src, width = "auto", className }) => (
-  <img src={src} width={width} className={className} />
-);
+export const ImageHeader: FC<IProps> = ({
+  src,
+  width = "auto",
+  className = "",
+  onClick
+}) => <img src={src} width={width} className={className} onClick={onClick} />;
