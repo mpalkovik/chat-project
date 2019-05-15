3c1be4fe6feb0e801687d9ae62472a60c4bb429f
kristina kolesarova <kristina.kolesarova@akademiasovy.sk>
2019-05-15 19:40:57 +0200

    fixed

--- src/components/Homepage/Button.tsx
+++ src/components/Homepage/Button.tsx
@@ -4,7 +4,10 @@ interface IProps {
   className?: string;
 }
 
-export const Button: FC<IProps> = ({ children, className = "text-white font-lato font-bold" }) => (
+export const Button: FC<IProps> = ({
+  children,
+  className = "text-white font-lato font-bold"
+}) => (
   <button className="w-full h-10 flex justify-center items-center">
     <div className={className}>{children}</div>
   </button>
--- src/components/Homepage/Description.tsx
+++ src/components/Homepage/Description.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from "react";
 
-
 export const Description: FC = ({ children }) => (
   <div className="leading-tight">
     <h1 className="font-gilbert text-4xl">
