3837aef2deccb262652541dc3a71b198fc3f6f67
erik.ruszinka <erik.ruszinka@gmail.com>
2019-05-16 09:51:40 +0200

    Deleted space

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface IProps {
 export const Button: FC<IProps> = ({ isInverted = false, children }) => (
   <button
     className={classnames(
-      "mt-1 h-10 w-full font-lato text-base hover:shadow-lg active:opacity-50 mr-20 font-bold  border-solid border border-black rounded-xl",
+      "mt-1 h-10 w-full font-lato text-base hover:shadow-lg active:opacity-50 mr-20 font-bold border-solid border border-black rounded-xl",
       {
         [`${
           isInverted
