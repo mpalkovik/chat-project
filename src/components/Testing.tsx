2b335ac89af1c3e4cc35068ac7c032ab55ffc3e7
Kristina <kristina.kolesarova@globallogic.com>
2019-05-17 10:06:40 +0200

    fixed

--- src/components/Testing.tsx
+++ src/components/Testing.tsx
@@ -1,9 +1,4 @@
 import React, { FC } from "react";
 import { RouteComponentProps } from "@reach/router";
-import { Footer } from "../components/footer";
 
-export const Testing: FC<RouteComponentProps> = () => (
-  <div>
-    <Footer />
-  </div>
-);
+export const Testing: FC<RouteComponentProps> = () => <div />;
