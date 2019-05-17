3b9c9066f3877890121f6fdd6252c9293995f2eb
slavomircesla <slavomir.cesla@akademiasovy.sk>
2019-05-17 11:45:17 +0200

    done

--- src/components/Thumbnail/Description.tsx
+++ src/components/Thumbnail/Description.tsx
@@ -5,15 +5,13 @@ interface IProps {
   classname?: string;
 }
 export const Description: FC<IProps> = ({ children, classname }) => (
-  <div>
-    <p
-      style={{
-        height: "36px",
-        lineHeight: "18px"
-      }}
-      className={classnames("text-sm font-lato w-auto", classname)}
-    >
-      {children}
-    </p>
-  </div>
+  <p
+    style={{
+      height: "36px",
+      lineHeight: "18px"
+    }}
+    className={classnames("text-sm font-lato w-auto", classname)}
+  >
+    {children}
+  </p>
 );
--- src/components/Thumbnail/Name.tsx
+++ src/components/Thumbnail/Name.tsx
@@ -1,10 +1,5 @@
 import React, { FC } from "react";
 
 export const Name: FC = ({ children }) => (
-  <span
-    className="font-gilbert flex text-xl text-black w-full"
-    style={{ height: "28px" }}
-  >
-    {children}
-  </span>
+  <p className="font-gilbert text-xl text-black w-full">{children}</p>
 );
