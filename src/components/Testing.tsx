72b28b0f00763006eda1215058fa74099e466d97
PavolKiss <pavol.kiss@akademiasovy.sk>
2019-05-20 12:28:01 +0200

    remove header from testing

--- src/components/Testing.tsx
+++ src/components/Testing.tsx
@@ -1,5 +1,4 @@
 import React, { FC } from "react";
 import { RouteComponentProps } from "@reach/router";
-import { Header } from "./Header/Header";
 
-export const Testing: FC<RouteComponentProps> = () => <Header />;
+export const Testing: FC<RouteComponentProps> = () => <div />;
