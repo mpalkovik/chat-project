67b85e1ae8eb07483284704e278b15424bb24c39
Stefan Rothmajer <stefan.rothmajer@akademiasovy.sk>
2019-05-21 13:24:49 +0200

    Changed maximal width of content on the page

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { FC } from "react";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer";
 import background from "../../static/svg/backgroundImage.svg";
 import { BackgroundImage } from "../Homepage/BackgroundImage";
-interface IProps {
-  children?: React.ReactNode;
-}
-export const Layout = (props: IProps) => (
+
+export const Layout: FC = props => (
   <>
     <Header />
     <BackgroundImage url={background} isCover>
-      <div style={{ maxWidth: "1280px" }}>{props.children}</div>
+      <div className="flex justify-center w-full">
+        <div style={{ maxWidth: "1280px" }}>{props.children}</div>
+      </div>
     </BackgroundImage>
     <Footer />
   </>
