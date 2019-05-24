87f2b2e7915b55bcde80db2919be661ee554e382
Peter Guzma <peter.guzma@globallogic.com>
2019-05-24 13:22:28 +0200

    update leading

--- src/components/Thumbnail/Description.tsx
+++ src/components/Thumbnail/Description.tsx
@@ -5,7 +5,7 @@ interface IProps {
   className?: string;
 }
 export const Description: FC<IProps> = ({ children, className }) => (
-  <p className={classnames("text-12 font-lato h-40 leading-less", className)}>
+  <p className={classnames("text-12 font-lato h-40 leading-18", className)}>
     {children}
   </p>
 );
