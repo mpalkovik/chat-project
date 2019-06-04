a076226b6ac533574b9857d947dc4a480395b07c
mpalkovik <martin.palkovik@globallogic.com>
2019-06-04 15:18:15 +0000

    added comments

--- clear-frontend.sh
+++ clear-frontend.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-OPEN_PR=$(curl -s https://api.github.com/repos/mpalkovik/chat-project/pulls?state=open | jq ".[].number")
+# get PR requests
+#OPEN_PR=$(curl -s https://api.github.com/repos/mpalkovik/chat-project/pulls?state=open | jq ".[].number")
 CLOSED_PR=$(curl -s https://api.github.com/repos/mpalkovik/chat-project/pulls?state=closed | jq ".[].number")
 
+# check for running closed PR requests
 for pr in $CLOSED_PR
 do
    to_clean=$(docker ps | grep -wo "chat-project_node:$pr" | cut -d ':' -f2)
+# clean closed PR requests
    if [ "$pr" == "$to_clean" ];then
       echo "cleaning chat-project-v_$to_clean"
       docker-compose -p chat-project-v_$to_clean down
