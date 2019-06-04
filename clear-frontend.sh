9496249099b923652fbad3ee93ee627c9805e659
mpalkovik <martin.palkovik@globallogic.com>
2019-06-04 11:39:32 +0000

    working clean script

--- clear-frontend.sh
+++ clear-frontend.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 #ghprbPullId=$1
-VERSION=$1
+#VERSION=$1
 
 ### curl -s https://api.github.com/repos/garySiladi/chat-project/pulls?state=open | jq '.[] | "\(.number) \(.head.sha)"'
 
-#OPEN_PR=${curl -u "mpalkovik" https://api.github.com/repos/Stephaaan/chat_backend/pulls?state=open | jq ".[].number"}
-#CLOSED_PR=${curl -u "mpalkovik" https://api.github.com/repos/Stephaaan/chat_backend/pulls?state=closed | jq ".[].number"}
-OPEN_PR="7"
-CLOSED_PR="5 6"
+OPEN_PR=$(curl -s https://api.github.com/repos/mpalkovik/chat-project/pulls?state=open | jq ".[].number")
+CLOSED_PR=$(curl -s https://api.github.com/repos/mpalkovik/chat-project/pulls?state=closed | jq ".[].number")
+#OPEN_PR="11"
+#CLOSED_PR="9"
+#CLOSED_PR="9 10"
 
-echo -e "open: $OPEN_PR\nclosed: $CLOSED_PR"
+#echo -e "open: $OPEN_PR\nclosed: $CLOSED_PR"
 # get open pr and running pr
 # to run - open pr not running
 # to kill - closed pr running
 
 for pr in $CLOSED_PR
 do
-   docker ps | grep "chat-project_node:$pr" | tr -s ' ' | cut -d ' ' -f1
+#   to_clean=$(docker ps | grep "chat-project_node:$pr" | tr -s ' ' | cut -d ' ' -f2 | cut -d ':' -f2)
+   to_clean=$(docker ps | grep -wo "chat-project_node:$pr" | cut -d ':' -f2)
+#   echo $to_clean
+   if [ "$pr" == "$to_clean" ];then
+      echo "cleaning chat-project-v_$to_clean"
+      docker-compose -p chat-project-v_$to_clean down
+#   else
+#      echo "nothing to clean"
+   fi
 done
+
