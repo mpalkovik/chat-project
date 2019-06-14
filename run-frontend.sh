b4c940eb66ac3dc8a5109e7f8ba3876ffc2d4887
mpalkovik <martin.palkovik@globallogic.com>
2019-06-14 10:16:38 +0200

    added .env file and variables check

--- run-frontend.sh
+++ run-frontend.sh
@@ -2,13 +2,25 @@
 REPO_DIR=$(pwd)
 VERSION=$1
 
+# check if .env file exists
+if [ ! -f ".env" ];then
+   echo "DOCKER_VERSION=0" > .env
+   echo "DOCKER_PORT_NODE=11000" >> .env
+elif [ -z $(cat .env | grep DOCKER_VERSION) ];then
+   echo "" >> .env
+   echo "DOCKER_VERSION=0" >> .env
+elif [ -z $(cat .env | grep DOCKER_PORT_NODE) ];then
+   echo "" >> .env
+   echo "DOCKER_PORT_NODE=11000" >> .env
+fi
+
 # check $VERSION variable and create docker-compose .env file
 if [ ! -z $VERSION ];then
-   sed -i "s/DOCKER_VERSION=[0-9]*/DOCKER_VERSION=$VERSION/" .env
+   sed -i "s/DOCKER_VERSION=latest\|DOCKER_VERSION=[0-9]*/DOCKER_VERSION=$VERSION/" .env
    sed -i "s/DOCKER_PORT_NODE=[0-9]*/DOCKER_PORT_NODE=$(( 11000 + $VERSION ))/" .env
 else
    VERSION="latest"
-   sed -i "s/DOCKER_VERSION=[0-9]*/DOCKER_VERSION=$VERSION/" .env
+   sed -i "s/DOCKER_VERSION=latest\|DOCKER_VERSION=[0-9]*/DOCKER_VERSION=$VERSION/" .env
    sed -i "s/DOCKER_PORT_NODE=[0-9]*/DOCKER_PORT_NODE=11000/" .env
 fi
 
